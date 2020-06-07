78b83e9dca958442c660c753991b93626657c485
archna-dev <63065633+archna-dev@users.noreply.github.com>
2020-06-06 22:33:36 -0700

    cast updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,18 @@ import { TvService } from './tv.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tv-app';
-  tvapp: ITvapp;
+  tvapp: ITvappcast;
 
-  constructor(private Tvservice: TvService){}
-  doSearch(searchValue){
-    const userInput = searchValue.split(',')
-                      .map(s => s.trim())
-    this.Tvservice.getTvapp(userInput[0]).
-    subscribe(data => this.tvapp = data);
+  constructor(private tvService: TvService) {}
+
+  doSearch(searchValue) {
+    const userInput = searchValue.split(',').map((s) => s.trim());
+    this.tvService
+      .getTvappcast(userInput[0])
+      .subscribe((data) => (this.tvapp = data));
   }
 }
-
-
--- src/app/tv-app/tv-app.component.ts
+++ src/app/tv-app/tv-app.component.ts
@@ -1,19 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ITvapp, ITvappcast } from '../itvapp';
 
-
 @Component({
   selector: 'app-tv-app',
   templateUrl: './tv-app.component.html',
-  styleUrls: ['./tv-app.component.css']
+  styleUrls: ['./tv-app.component.css'],
 })
 export class TvAppComponent implements OnInit {
-  @Input() current: ITvapp;
-  constructor(){}
+  @Input() current: ITvappcast;
+  constructor() {}
 
-  ngOnInit(): void{
-
-  }
+  ngOnInit(): void {}
 }
-
-
